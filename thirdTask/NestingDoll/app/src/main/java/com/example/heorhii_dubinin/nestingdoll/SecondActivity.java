c8af83674dc7ff2a646dab85db77ebe65f84aef1
Heorhii Dubinin <heorhii_dubinin@epam.com>
2018-10-23 15:25:05 +0300

    made little refactoring

--- thirdTask/NestingDoll/app/src/main/java/com/example/heorhii_dubinin/nestingdoll/SecondActivity.java
+++ thirdTask/NestingDoll/app/src/main/java/com/example/heorhii_dubinin/nestingdoll/SecondActivity.java
@@ -1,63 +1,16 @@
 package com.example.heorhii_dubinin.nestingdoll;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private static final String TAG = "SecondActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        loggingAndToasting("onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-    }
-
-    @Override
-    protected void onStart() {
-        loggingAndToasting("onStart");
-        super.onStart();
-    }
-
-    @Override
-    protected void onStop() {
-        loggingAndToasting("onStop");
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        loggingAndToasting("onDestroy");
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause() {
-        loggingAndToasting("onPause");
-        super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        loggingAndToasting("onResume");
-        super.onResume();
-    }
-
-    @Override
-    protected void onRestart() {
-        loggingAndToasting("onRestart");
-        super.onRestart();
-    }
-
-    public void loggingAndToasting(String msg) {
-        Toast toast = Toast.makeText(this, msg + " " + TAG, Toast.LENGTH_SHORT);
-        toast.show();
-        Log.v(TAG, msg);
     }
 
     public void openThirdActivity(View v) {
